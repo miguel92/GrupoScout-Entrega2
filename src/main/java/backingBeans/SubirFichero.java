becab488b0361638d03d5df2814d1348bcda3741
migue <migue@192.168.1.128>
2018-05-05 17:43:09 +0200

    Documentos y Mis datos

---
+++ src/main/java/backingBeans/SubirFichero.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backingBeans;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author migue
+ */
+@Named(value = "subirFichero")
+@RequestScoped
+public class SubirFichero {
+	private Part uploadedFile;
+	private String folder = "c:\\files";
+
+	public Part getUploadedFile() {
+		return uploadedFile;
+	}
+
+	public void setUploadedFile(Part uploadedFile) {
+		this.uploadedFile = uploadedFile;
+	}
+
+	
+	public void saveFile(){
+		
+		try (InputStream input = uploadedFile.getInputStream()) {
+			String fileName = uploadedFile.getSubmittedFileName();
+	        Files.copy(input, new File(folder, fileName).toPath());
+	    }
+	    catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+}
---
+++ src/main/java/backingBeans/misDocs.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backingBeans;
+
+import com.softbox.gruposantoangel.entity.Documento;
+import com.softbox.gruposantoangel.entity.Documento_Plantilla;
+import com.softbox.gruposantoangel.entity.EstadoDoc;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
+
+/**
+ *
+ * @author migue
+ */
+@Named(value = "misDocs")
+@SessionScoped
+public class misDocs implements Serializable{
+    private Long nextDoc = Long.parseLong("1");
+    private List<Documento> lista_docs;
+    private Documento miDoc;
+    /**
+     * Creates a new instance of misDocs
+     */
+    public misDocs() {
+        lista_docs = new ArrayList<>();
+        miDoc = new Documento();
+        miDoc.setId_documento(nextDoc++);
+        miDoc.setFecha(Date.valueOf("2018-02-19"));
+        miDoc.setEnlace("C:\\files\\578025.png");
+        miDoc.setDoc_plantilla(new Documento_Plantilla());
+        miDoc.setNombre("Alergias");
+        miDoc.setTipo("1");
+        miDoc.setEstado(EstadoDoc.ACEPTADO);
+        lista_docs.add(miDoc);
+        
+        miDoc = new Documento();
+        miDoc.setId_documento(nextDoc++);
+        miDoc.setFecha(Date.valueOf("2017-07-19"));
+        miDoc.setEnlace("./resources/docs/dni.pdf");
+        miDoc.setDoc_plantilla(new Documento_Plantilla());
+        miDoc.setNombre("DNI");
+        miDoc.setEstado(EstadoDoc.ENTREGADO);
+        miDoc.setTipo("2");
+        lista_docs.add(miDoc);
+        
+    }
+
+    public Long getNextDoc() {
+        return nextDoc;
+    }
+
+    public void setNextDoc(Long nextDoc) {
+        this.nextDoc = nextDoc;
+    }
+
+    public List<Documento> getLista_docs() {
+        return lista_docs;
+    }
+
+    public void setLista_docs(List<Documento> lista_docs) {
+        this.lista_docs = lista_docs;
+    }
+
+    public Documento getMiDoc() {
+        return miDoc;
+    }
+
+    public void setMiDoc(Documento miDoc) {
+        this.miDoc = miDoc;
+    }
+    
+    public String borrarDoc(){
+        return "index.xhtml";
+        /*boolean encontrado = false;
+        int contador = 0;
+        Documento temp = null;
+                        System.out.println("ASDASDASDASDASDSAD");
+                System.out.println(temp);
+        while(contador < lista_docs.size() && !encontrado){
+            temp = lista_docs.get(contador);
+            if(doc.getId_documento().compareTo(temp.getId_documento())==0){
+                System.out.println("ASDASDASDASDASDSAD");
+                System.out.println(temp);
+                encontrado = true;
+                lista_docs.remove(contador);
+            }
+            contador++;
+        }*/
+        
+    }
+}
