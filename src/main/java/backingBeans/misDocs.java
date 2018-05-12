96a8b8e9f80923f98f71bd8b0bfa9bacb161bf00
otman <otman@192.168.1.42>
2018-05-12 19:09:53 +0200

    Documentos y notificaciones.

--- src/main/java/backingBeans/misDocs.java
+++ src/main/java/backingBeans/misDocs.java
@@ -7,11 +7,13 @@ package backingBeans;
 
 import com.softbox.gruposantoangel.entity.Documento;
 import com.softbox.gruposantoangel.entity.EstadoDoc;
+import com.softbox.gruposantoangel.entity.Socio;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.sql.Date;
 import java.util.List;
 import javax.inject.Named;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.context.SessionScoped;
 
 /**
@@ -28,31 +30,51 @@ public class misDocs implements Serializable{
      * Creates a new instance of misDocs
      */
     public misDocs() {
+        Socio socio1 = new Socio();
+        socio1.setId_Socio(1000L);
         lista_docs = new ArrayList<>();
         miDoc = new Documento();
         miDoc.setId_documento(nextDoc++);
         miDoc.setFecha(Date.valueOf("2018-02-19"));
-        miDoc.setEnlace("C:\\files\\578025.png");
+        miDoc.setEnlace("http://localhost:8080/GrupoScout-Entrega2-master/faces/Ficheros/578025.png");
         miDoc.setNombre("Alergias");
         miDoc.setTipo("1");
-        miDoc.setEstado(EstadoDoc.ACEPTADO);
+        miDoc.setEstado(EstadoDoc.ENTREGADO);
+        miDoc.setSocio(socio1);
         lista_docs.add(miDoc);
         
+        Socio socio2 = new Socio();
+        socio2.setId_Socio(1001L);
         miDoc = new Documento();
         miDoc.setId_documento(nextDoc++);
         miDoc.setFecha(Date.valueOf("2017-07-19"));
-        miDoc.setEnlace("./resources/docs/dni.pdf");
+        miDoc.setEnlace("http://localhost:8080/GrupoScout-Entrega2-master/faces/Ficheros/dni.pdf");
         miDoc.setNombre("DNI");
         miDoc.setEstado(EstadoDoc.ENTREGADO);
         miDoc.setTipo("2");
+        miDoc.setSocio(socio2);
         lista_docs.add(miDoc);
-        
     }
-
+    
+    
+    public void subirFichero(Documento doc){
+        lista_docs.add(doc);
+    }
+    public String getEnlace(Documento doc){
+        return doc.getEnlace();
+    }
     public Long getNextDoc() {
         return nextDoc;
     }
-
+    
+    public void validarDoc (Documento doc){
+        doc.setEstado(EstadoDoc.ACEPTADO);
+    }
+    
+    public void rechazarDoc (Documento doc){
+        doc.setEstado(EstadoDoc.DENEGADO);
+    }
+    
     public void setNextDoc(Long nextDoc) {
         this.nextDoc = nextDoc;
     }
@@ -73,23 +95,19 @@ public class misDocs implements Serializable{
         this.miDoc = miDoc;
     }
     
-    public String borrarDoc(Documento doc){
-        
+    public void borrarDoc(Documento doc){
+        //return "index.xhtml";
         boolean encontrado = false;
         int contador = 0;
         Documento temp = null;
-
         while(contador < lista_docs.size() && !encontrado){
             temp = lista_docs.get(contador);
             if(doc.getId_documento().compareTo(temp.getId_documento())==0){
                 encontrado = true;
-                lista_docs.remove(contador);
+                lista_docs.remove(doc);
             }
             contador++;
         }
-        return "misDocumentos.xhtml";
-    }
-    public String getEnlace(Documento doc){
-        return doc.getEnlace();
+        
     }
 }
--- src/main/java/backingBeans/notificacionesBB.java
+++ src/main/java/backingBeans/notificacionesBB.java
@@ -5,7 +5,15 @@
  */
 package backingBeans;
 
+import com.softbox.gruposantoangel.entity.Evento;
+import com.softbox.gruposantoangel.entity.Notificacion_Documento;
+import com.softbox.gruposantoangel.entity.Notificacion_Evento;
+import com.softbox.gruposantoangel.entity.Socio;
 import java.io.Serializable;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 
@@ -16,14 +24,54 @@ import javax.enterprise.context.SessionScoped;
 @Named(value = "notificacionesBB")
 @SessionScoped
 public class notificacionesBB implements Serializable{
-
+    
+   
     /**
      * Creates a new instance of notificacionesBB
      */
     private int noLeidas;
+    private Long sigIdNotificacion = Long.parseLong("1");
+    private Long sigIdEvento = Long.parseLong("1");
+    private Long sigIdSocio = Long.parseLong("1000");
+    private List<Notificacion_Evento> notif_eventos;
+    private Notificacion_Evento notificacion = new Notificacion_Evento();
     
     public notificacionesBB() {
-        noLeidas = 5;
+        this.noLeidas = 2;
+        notif_eventos = new ArrayList<Notificacion_Evento>();
+        Notificacion_Evento notif1 = new Notificacion_Evento();
+        Evento ev1 = new Evento();
+        ev1.setId_evento(sigIdEvento++);
+        ev1.setNombre("Visita a Antequera");
+        ev1.setDescripcion("Se visitaran las cuevas y los bosques.");
+        
+        ev1.setFecha(Date.valueOf("2018-07-15"));
+        Socio socio1 = new Socio();
+        socio1.setId_Socio(sigIdSocio++);
+        notif1.setId_not_evento(sigIdNotificacion++);
+        notif1.setEvento(ev1);
+        notif1.setId_not_evento(ev1.getId_evento());
+        notif1.setSocio(socio1);
+        notif1.setFechaNotificacion(Date.valueOf("2018-05-12"));
+        notif1.setEstado("False");
+        notif_eventos.add(notif1);
+        
+        Notificacion_Evento notif2 = new Notificacion_Evento();
+        Evento ev2 = new Evento();
+        ev2.setId_evento(sigIdEvento++);
+        ev2.setNombre("Actividad en el campamento de Huelva");
+        ev2.setDescripcion("Instancia de tres dias en el campamento");
+        ev2.setFecha(Date.valueOf("2018-08-05"));
+        Socio socio2 = new Socio();
+        socio2.setId_Socio(sigIdSocio++);
+        notif2.setId_not_evento(sigIdNotificacion++);
+        notif2.setEvento(ev2);
+        notif2.setId_not_evento(ev2.getId_evento());
+        notif2.setSocio(socio2);
+        notif2.setFechaNotificacion(Date.valueOf("2018-05-09"));
+        notif2.setEstado("False");
+        notif_eventos.add(notif2);
+        
     }
 
     public int getNoLeidas() {
@@ -34,4 +82,26 @@ public class notificacionesBB implements Serializable{
         this.noLeidas = noLeidas;
     }
     
+    public List<Notificacion_Evento> getNotifEventos(){
+        return notif_eventos;
+    }
+    
+    public void setNotif_Eventos(List<Notificacion_Evento> notif){
+        this.notif_eventos = notif;
+    }
+    
+    public Notificacion_Evento getNotificacion_Evento(){
+        return notificacion;
+    }
+    
+
+    public void setNotificacion_Evento(Notificacion_Evento notif) {
+        this.notificacion = notif;
+    }
+    
+    //Accede a la vista de creación de socios
+    
+    
+    //Crea el socio con los datos proporcionado en la vista de creación
+
 }
